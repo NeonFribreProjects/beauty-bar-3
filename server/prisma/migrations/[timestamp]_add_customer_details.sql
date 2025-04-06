fc2912155464c17f34859e22931351e787da7066
NeonFribreProjects <neonfibre@outlook.com>
2025-04-06 22:52:18 +0100

    Check point for the Hot Reloading 6 look at the 1 commit

--- server/prisma/migrations/[timestamp]_add_customer_details.sql
+++
@@ -1,8 +0,0 @@
--- Delete existing bookings
-DELETE FROM "Booking";
-
--- Add new required columns
-ALTER TABLE "Booking" 
-ADD COLUMN "customerName" TEXT NOT NULL,
-ADD COLUMN "customerEmail" TEXT NOT NULL,
-ADD COLUMN "customerPhone" TEXT NOT NULL; 
