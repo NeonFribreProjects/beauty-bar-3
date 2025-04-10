8d897dc7b177fe04c48481476721a5ab617e4a8b
NeonFribreProjects <neonfibre@outlook.com>
2025-04-10 03:48:28 +0100

    Fixed the port issue here check code some syntax error

--- server/src/index.ts
+++ server/src/index.ts
@@ -55,7 +55,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = parseInt(process.env.PORT || '3000', 10);
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://0.0.0.0:${PORT}`);
 }); 
